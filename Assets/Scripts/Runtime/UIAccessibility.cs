dc734582c98630777888945c4c9e4ca8fe169a3e
Brakcc <124096980+Brakcc@users.noreply.github.com>
2026-04-29 19:09:30 +0200

    alors

--- Assets/Scripts/Runtime/UIAccessibility.cs
+++ Assets/Scripts/Runtime/UIAccessibility.cs
@@ -19,8 +19,7 @@ namespace Runtime
         {
             GetComponent<PanelRenderer>().UnregisterUIReloadCallback(OnReloadUI);
             
-            _settingsButton.UnregisterCallback<ClickEvent>(Settings);
-            _backButton.UnregisterCallback<ClickEvent>(Settings);
+            OnUnregisterButtons();
         }
         
         #endregion
@@ -32,35 +31,112 @@ namespace Runtime
             _root = root;
             _panel = panel;
             
-            _settingsButton = root.Q<Button>("Settings");
+            OnRegisterPanels();
+            OnRegisterButtons();
+        }
+
+        private void OnRegisterPanels()
+        {
+            _mainPanel = _root.Q<VisualElement>("MainPanel");
+            _settingsSubPanel = _root.Q<VisualElement>("SettingsSubPanel");
+            _graphicsPanel = _root.Q<VisualElement>("GraphicsPanel");
+            _soundPanel = _root.Q<VisualElement>("SoundPanel");
+            _textsPanel = _root.Q<VisualElement>("TextsPanel");
+        }
+
+        private void OnRegisterButtons()
+        {
+            _startGameButton = _root.Q<Button>("Start");
+            _startGameButton.RegisterCallback<ClickEvent>(StartGame);
+            
+            _settingsButton = _root.Q<Button>("Settings");
             _settingsButton.RegisterCallback<ClickEvent>(Settings);
             
-            _backButton = root.Q<Button>("BackFromSettings");
-            _backButton.RegisterCallback<ClickEvent>(Settings);
+            _backFromSubButton = _root.Q<Button>("BackFromSubSet");
+            _backFromSubButton.RegisterCallback<ClickEvent>(Settings);
+            
+            _quitGameButton = _root.Q<Button>("Quit");
+            _quitGameButton.RegisterCallback<ClickEvent>(QuitGame);
+            
+            _soundsButton = _root.Q<Button>("Sounds");
+            _soundsButton.RegisterCallback<ClickEvent>(Sounds);
+            
+            _backFromSoundsButton = _root.Q<Button>("BackFromSounds");
+            _backFromSoundsButton.RegisterCallback<ClickEvent>(Sounds);
+            
+            _textsButton = _root.Q<Button>("Texts");
+            _textsButton.RegisterCallback<ClickEvent>(Texts);
+            
+            _backFromTextsButton = _root.Q<Button>("BackFromTexts");
+            _backFromTextsButton.RegisterCallback<ClickEvent>(Texts);
+            
+            _graphicsButton = _root.Q<Button>("Graphics");
+            _graphicsButton.RegisterCallback<ClickEvent>(Graphics);
+            
+            _backFromGraphicsButton = _root.Q<Button>("BackFromGraphics");
+            _backFromGraphicsButton.RegisterCallback<ClickEvent>(Graphics);
+        }
+
+        private void OnUnregisterButtons()
+        {
+            _startGameButton.UnregisterCallback<ClickEvent>(StartGame);
+            _settingsButton.UnregisterCallback<ClickEvent>(Settings);
+            _backFromSubButton.UnregisterCallback<ClickEvent>(Settings);
+            _quitGameButton.UnregisterCallback<ClickEvent>(QuitGame);
+            _soundsButton.UnregisterCallback<ClickEvent>(Sounds);
+            _backFromSoundsButton.UnregisterCallback<ClickEvent>(Sounds);
+            _textsButton.UnregisterCallback<ClickEvent>(Texts);
+            _backFromTextsButton.UnregisterCallback<ClickEvent>(Texts);
+            _graphicsButton.UnregisterCallback<ClickEvent>(Graphics);
+            _backFromGraphicsButton.UnregisterCallback<ClickEvent>(Graphics);
         }
 
         #endregion
         
         #region commands
 
-        private void Settings(ClickEvent evt)
-        {
-            _root[1].enabledSelf = !_root[1].enabledSelf;
-            _root[0].enabledSelf = !_root[0].enabledSelf;
-        }
-
-        private void StartGame()
+        #region navigation
+        
+        private void StartGame(ClickEvent evt)
         {
             Debug.Log("Starting game");
         }
 
-        private void QuitGame()
+        private void QuitGame(ClickEvent evt)
         {
             Application.Quit();
 #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
 #endif
         }
+        
+        private void Settings(ClickEvent evt)
+        {
+            _mainPanel.enabledSelf = !_mainPanel.enabledSelf;
+            _settingsSubPanel.enabledSelf = !_settingsSubPanel.enabledSelf;
+        }
+
+        private void Sounds(ClickEvent evt)
+        {
+            _soundPanel.enabledSelf = !_soundPanel.enabledSelf;
+            _settingsSubPanel.enabledSelf = !_settingsSubPanel.enabledSelf;
+        }
+
+        private void Texts(ClickEvent evt)
+        {
+            _textsPanel.enabledSelf = !_textsPanel.enabledSelf;
+            _settingsSubPanel.enabledSelf = !_settingsSubPanel.enabledSelf;
+        }
+
+        private void Graphics(ClickEvent evt)
+        {
+            _graphicsPanel.enabledSelf = !_graphicsPanel.enabledSelf;
+            _settingsSubPanel.enabledSelf = !_settingsSubPanel.enabledSelf;
+        }
+        
+        #endregion
+        
+        #region sounds
         
         private void SetMasterVolume(ChangeEvent<float> evt)
         {
@@ -76,6 +152,26 @@ namespace Runtime
         {
             Screen.fullScreen = value;
         }
+        
+        #endregion
+        
+        #region graphics
+
+        private static void ToggleFullscreen(bool value)
+        {
+            Screen.fullScreen = value;
+        }
+        
+        #endregion
+        
+        #region texts
+
+        private void SetFontSize(ChangeEvent<float> evt)
+        {
+            
+        }
+        
+        #endregion
 
         #endregion
 
@@ -89,9 +185,61 @@ namespace Runtime
         
         private VisualElement _root;
 
+        #region panels
+        
+        private VisualElement _mainPanel;
+        
+        private VisualElement _settingsSubPanel;
+
+        private VisualElement _soundPanel;
+
+        private VisualElement _graphicsPanel;
+        
+        private VisualElement _textsPanel;
+
+        #endregion
+        
+        #region navigation
+        
+        private Button _startGameButton;
+        
+        private Button _quitGameButton;
+        
         private Button _settingsButton;
 
-        private Button _backButton;
+        private Button _backFromSubButton;
+        
+        private Button _soundsButton;
+        
+        private Button _backFromSoundsButton;
+        
+        private Button _graphicsButton;
+        
+        private Button _backFromGraphicsButton;
+        
+        private Button _textsButton;
+        
+        private Button _backFromTextsButton;
+        
+        #endregion
+        
+        #region sounds
+        
+        
+        
+        #endregion
+        
+        #region graphics
+        
+        
+        
+        #endregion
+        
+        #region texts
+        
+        
+        
+        #endregion
 
         #endregion
     }
